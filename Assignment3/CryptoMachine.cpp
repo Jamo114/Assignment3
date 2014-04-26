460825b885b5c0096ac7bb8fdbf3785ea15bb5b9
James Oosterbaan <jamesoosterbaan@gmail.com>
2014-04-26 16:32:01 +0200

    Adding the grouping function for strings.

--- Assignment3/CryptoMachine.cpp
+++ Assignment3/CryptoMachine.cpp
@@ -1,6 +1,6 @@
 #include "CryptoMachine.h"
 
-
+using namespace std;
 CryptoMachine::CryptoMachine()
 {
 }
@@ -8,4 +8,41 @@ CryptoMachine::CryptoMachine()
 
 CryptoMachine::~CryptoMachine()
 {
+}
+
+//Removes all spaces and adds a space after every fifth character
+string CryptoMachine::Group(string x)
+{
+	plainText = x;
+	int i = 0;
+	int j = 0;
+	while (i < plainText.length())
+	{
+		if (plainText[i] == ' ') plainText.erase(i, 1);
+		else i++;
+	}
+	i = 0;
+	j = 0;
+	int count = 0;
+	string plainTextTemp;
+	int newSize = plainText.length() + (plainText.length() / 5);
+	plainTextTemp.resize(newSize, ' ');
+	while (i < plainText.length())
+	{
+		if (count == 5)
+		{
+			plainTextTemp[j] = ' ';
+			++j;
+			count = 0;
+		}
+		else
+		{
+			plainTextTemp[j] = plainText[i];
+			++i;
+			++j;
+			++count;
+		}
+	}
+	plainText = plainTextTemp;
+	return plainText;
 }
--- Assignment3/CryptoMachine.h
+++ Assignment3/CryptoMachine.h
@@ -1,8 +1,17 @@
-#pragma once
+#ifndef CRYPTOMACHINE_H_
+#define CRYPTOMACHINE_H_
+
+#include <string>
+#include <iostream>
+#include <algorithm>
 class CryptoMachine
 {
 public:
 	CryptoMachine();
 	~CryptoMachine();
+	std::string Group(std::string);
+
+	std::string plainText;
 };
 
+#endif
